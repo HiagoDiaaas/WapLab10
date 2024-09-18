eaaac2b1df7a9525ed0c061523d9a9939f02336c
Hiago Dias <51371403+HiagoDiaaas@users.noreply.github.com>
2024-09-18 06:55:37 -0500

    display comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import './App.scss'
-import avatar from './images/bozai.png'
-
+import './App.scss';
+import avatar from './images/bozai.png';
 
 // Comment List data
 const defaultList = [
   {
-    // comment id
     rpid: 3,
-    // user info
     user: {
       uid: '13258165',
       avatar: '',
       uname: 'Jay Zhou',
     },
-    // comment content
     content: 'Nice, well done',
-    // created datetime
     ctime: '10-18 08:15',
     like: 88,
   },
@@ -41,22 +36,20 @@ const defaultList = [
     ctime: '10-19 09:00',
     like: 66,
   },
-]
+];
+
 // current logged in user info
 const user = {
-  // userid
   uid: '30009257',
-  // profile
   avatar,
-  // username
   uname: 'John',
-}
+};
 
 // Nav Tab
 const tabs = [
   { type: 'hot', text: 'Top' },
   { type: 'newest', text: 'Newest' },
-]
+];
 
 const App = () => {
   return (
@@ -66,13 +59,11 @@ const App = () => {
         <ul className="nav-bar">
           <li className="nav-title">
             <span className="nav-title-text">Comments</span>
-            {/* Like */}
-            <span className="total-reply">{10}</span>
+            <span className="total-reply">{defaultList.length}</span>
           </li>
           <li className="nav-sort">
-            {/* highlight class name： active */}
-            <span className='nav-item'>Top</span>
-            <span className='nav-item'>Newest</span>
+            <span className="nav-item">Top</span>
+            <span className="nav-item">Newest</span>
           </li>
         </ul>
       </div>
@@ -83,59 +74,57 @@ const App = () => {
           {/* current logged in user profile */}
           <div className="reply-box-avatar">
             <div className="bili-avatar">
-              <img className="bili-avatar-img" src={avatar} alt="Profile" />
+              <img className="bili-avatar-img" src={user.avatar} alt="Profile" />
             </div>
           </div>
           <div className="reply-box-wrap">
-            {/* comment */}
             <textarea
               className="reply-box-textarea"
               placeholder="tell something..."
             />
-            {/* post button */}
             <div className="reply-box-send">
-              <div className="send-text">post</div>
+              <div className="send-text">Post</div>
             </div>
           </div>
         </div>
+
         {/* comment list */}
         <div className="reply-list">
-          {/* comment item */}
-          <div className="reply-item">
-            {/* profile */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                />
+          {defaultList.map((comment) => (
+            <div key={comment.rpid} className="reply-item">
+              <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                  <img
+                    className="bili-avatar-img"
+                    src={comment.user.avatar || avatar}
+                    alt="Profile"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="content-wrap">
-              {/* username */}
-              <div className="user-info">
-                <div className="user-name">jack</div>
-              </div>
-              {/* comment content */}
-              <div className="root-reply">
-                <span className="reply-content">This is reply</span>
-                <div className="reply-info">
-                  {/* comment created time */}
-                  <span className="reply-time">{'2023-11-11'}</span>
-                  {/* total likes */}
-                  <span className="reply-time">Like:{100}</span>
-                  <span className="delete-btn">
-                    Delete
-                  </span>
+              <div className="content-wrap">
+                {/* username */}
+                <div className="user-info">
+                  <div className="user-name">{comment.user.uname}</div>
+                </div>
+                {/* comment content */}
+                <div className="root-reply">
+                  <span className="reply-content">{comment.content}</span>
+                  <div className="reply-info">
+                    {/* comment created time */}
+                    <span className="reply-time">{comment.ctime}</span>
+                    {/* total likes */}
+                    <span className="reply-time">Like: {comment.like}</span>
+                    <span className="delete-btn">Delete</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
